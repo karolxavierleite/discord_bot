0dcfa6648246d5d23700934c61c62eacf58f4da6
Karoline Leite <karolxavierleite@gmail.com>
2026-01-14 12:44:10 -0300

    changing bot purpose to professional mentor

--- README.md
+++ README.md
@@ -1,16 +1,13 @@
-# dbot
-Discord bot that answers like a pirate
+# Discord BOT
+Discord bot that answers like a professional mentor
 
 # .env 
 Update the .env with your discord bot token and the key from OpenAI<br>
 Token for the discord bot: https://discord.com/developers/applications<br>
 OpenAI key: https://platform.openai.com/account/api-keys
 
-# hello bot
-To start the bot, run the following commands:<br>
-python discord_only.py
 
-# pirate bot 
+# mentor bot
 run the following commands:<br>
 pip install -r requirements.txt<br>
 python mybot.py
--- mybot.py
+++ mybot.py
@@ -11,21 +11,14 @@ DISCORD_TOKEN = os.getenv('TOKEN')
 # Initialize the OpenAI client
 openai_client = OpenAI(api_key=OPENAI_KEY)
 
-def call_openai(question):
-    completion = openai_client.chat.completions.create(
-        model="gpt-4o",
-        messages=[
-             {
-                 "role": "user",
-                 "content": f"Respond like a pirate to the following question:  {question}",
-            },
-        ]
+def openai_response_api(question):
+    response_api = openai_client.responses.create(
+        model="gpt-5-nano-2025-08-07",
+        reasoning={"effort": "low"},
+        instructions="Responda a pergunta a seguir como uma mentora profissional que dá suporte a profissionais sêniores. Você é uma guru assertiva e direta, que responde com respostas curtas e claras, com poucos take aways para a pessoa ir atrás dos detalhes depois. No máximo 800 caracteres.",
+        input=question
     )
-    # Print the response
-    response = completion.choices[0].message.content
-    print(response)
-    return response
-
+    return response_api.output_text
 
 # Set up discord
 intents = discord.Intents.default()
@@ -47,8 +40,8 @@ async def on_message(message):
     if message.content.startswith('$question'):
         print(f"Message: {message.content}")                
         message_content = message.content.split("$question")[1]
-        print(f"Question: {message_content}")    
-        response = call_openai(message_content)   
+        print(f"Question: {message_content}")
+        response = openai_response_api(message_content)
         print(f"Assistant: {response}")    
         print("---")
         await message.channel.send(response)
